70c4eda9ee755eeaac08e7ffb1234dd98af59871
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-01-12 21:37:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ async def create_recover_markup(characteristic: str):
     markup.add(types.InlineKeyboardButton(text=f'Отримати 5 одиниць {characteristic} за 5 монет',
                                           callback_data=f'5{characteristic}5'))
     markup.add(types.InlineKeyboardButton(text=f'Отримати 15 одиниць {characteristic} за 13 монет',
-                                          callback_data=f'10{characteristic}13'))
+                                          callback_data=f'15{characteristic}13'))
     markup.add(types.InlineKeyboardButton(text=f'Отримати 50 одиниць {characteristic} за 45 монет',
                                           callback_data=f'50{characteristic}45'))
     markup.add(types.InlineKeyboardButton(text='Назад',
