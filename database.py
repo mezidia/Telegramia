42fa6d80a114d786d9bbb0b3499c57b90ef0f8ca
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-01-24 11:26:37 +0200

    Change Client class

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+# TODO: change to motor
 from pymongo import results, MongoClient
 
 
@@ -6,71 +7,75 @@ class Client:
     Class for manipulating MongoDB cluster
     """
 
-    def __init__(self, password, db_name: str, collection_name: str):
+    def __init__(self, password, db_name: str = 'Telegramia'):
         """
         Initializing client object with access to database
         :param password: password to account
         :param db_name: name of database in current cluster
-        :param collection_name: name of collection in current database
         """
         cluster = MongoClient(f'mongodb+srv://mezgoodle:{password}@telegramia.jkq5x.mongodb.net/'
                               f'telegramia?retryWrites=true&w=majority')
-        db = cluster[db_name]
-        self.collection = db[collection_name]
+        self.db = cluster[db_name]
 
-    def insert(self, data: dict) -> results.InsertOneResult:
+    def insert(self, data: dict, collection_name: str) -> results.InsertOneResult:
         """
         Method for inserting data in collection
+        :param collection_name: name of the collection
         :param data: dictionary with field name and value
         :return: result of inserting
         """
         try:
-            return self.collection.insert_one(data)
+            return self.db[collection_name].insert_one(data)
         except Exception as e:
             print('Error:', e)
 
-    def get(self, query: dict) -> dict:
+    def get(self, query: dict, collection_name: str) -> dict:
         """
         Method for getting data from collection
+        :param collection_name: name of the collection
         :param query: dictionary with field name and value
         :return: the document that matches the query
         """
         try:
-            return self.collection.find_one(query)
+            return self.db[collection_name].find_one(query)
         except Exception as e:
             print('Error:', e)
 
-    def get_all(self, query: dict = None) -> list:
+    def get_all(self, collection_name: str, query: dict = None) -> list:
         """
         Method for getting all data from collection
+        :param collection_name: name of the collection
+        :param query: dictionary with field name and value
         :return: the list of documents
         """
         if query is None:
             query = {}
         try:
-            return self.collection.find(query)
+            return self.db[collection_name].find(query)
         except Exception as e:
             print('Error:', e)
 
-    def update(self, query: dict, data: dict) -> results.UpdateResult:
+    def update(self, query: dict, data: dict, collection_name: str) -> results.UpdateResult:
         """
         Method for updating data in collection
+        :param collection_name: name of the collection
         :param query: dictionary with field name and value
         :param data: dictionary with the old field name in document and the new value
         :return: result of updating
         """
         try:
-            return self.collection.update_one(query, {'$set': data})
+            return self.db[collection_name].update_one(query, {'$set': data})
         except Exception as e:
             print('Error:', e)
 
-    def delete(self, query: dict) -> results.DeleteResult:
+    def delete(self, query: dict, collection_name: str) -> results.DeleteResult:
         """
         Method for deleting data in collection
+        :param collection_name: name of the collection
         :param query: dictionary with field name and value
         :return: result of deleting
         """
         try:
-            return self.collection.delete_one(query)
+            return self.db[collection_name].delete_one(query)
         except Exception as e:
             print('Error:', e)
--- test_database.py
+++ test_database.py
@@ -7,42 +7,42 @@ from config import DB_PASSWORD
 
 
 class Parent:
-    client = Client(DB_PASSWORD, 'Tests', 'tokens')
+    client = Client(DB_PASSWORD, 'Tests')
     full_data = {'test_token': 'test_token' + sys.version, 'test_id': 'test_id'}
     data_with_id = {'test_id': 'test_id'}
 
 
 class TestClient1(TestCase, Parent):
     def test_insert(self):
-        result = self.client.insert(self.full_data)
+        result = self.client.insert(self.full_data, 'tokens')
         self.assertTrue(result.acknowledged)
         self.assertIsNotNone(result.inserted_id)
 
 
 class TestClient2(TestCase, Parent):
     def test_get(self):
-        result = self.client.get({'test_token': 'test_token' + sys.version})
+        result = self.client.get({'test_token': 'test_token' + sys.version}, 'tokens')
         self.assertIsNotNone(result)
         self.assertIsInstance(result, dict)
         self.assertEqual(result['test_id'], self.full_data['test_id'])
 
     def test_get_all(self):
-        result = self.client.get_all()
+        result = self.client.get_all('tokens')
         self.assertIsNotNone(result)
 
     def test_update(self):
         result = self.client.update({'test_token': 'test_token' + sys.version}, {'test_token': 'test_token1' +
-                                                                                               sys.version})
+                                                                                               sys.version}, 'tokens')
         self.assertTrue(result.acknowledged)
         self.assertIsNotNone(result.modified_count)
-        updated_data = self.client.get(self.data_with_id)
+        updated_data = self.client.get(self.data_with_id, 'tokens')
         self.assertEqual(updated_data['test_token'], 'test_token1' + sys.version)
 
 
 class TestClient3(TestCase, Parent):
     def test_delete(self):
-        result = self.client.delete(self.data_with_id)
+        result = self.client.delete(self.data_with_id, 'tokens')
         self.assertIsNotNone(result)
         self.assertTrue(result.acknowledged)
-        data = self.client.get(self.data_with_id)
+        data = self.client.get(self.data_with_id, 'tokens')
         self.assertIsNone(data)
