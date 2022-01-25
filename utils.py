49693b24a6360f1f12f83186477636e891068fb6
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-01-25 11:58:14 +0200

    Update utils.py
    Create function for parsing purchase

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 def check_money(player: dict, price: float) -> bool:
     player_money = float(player['money'])
     return player_money - price > 0
@@ -6,3 +9,12 @@ def check_money(player: dict, price: float) -> bool:
 def check_characteristics(player: dict, value: float, characteristic: str) -> bool:
     player_value = player[characteristic] + value
     return player_value <= player['level'] * 50
+
+
+def parse_purchase(text: str) -> Tuple[str, float]:
+    text_list = text.split(' ')
+    price = float(text_list[-1])
+    item_list = text_list[1:-2]
+    item = ' '.join(item_list)
+    return item, price
+
