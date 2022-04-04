8e94c895b6a7b5eb1d7fdc9f3364c50438a2fe52
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-04 20:44:08 +0300

    Update commands.py

--- utils/misc/commands.py
+++ utils/misc/commands.py
@@ -2,13 +2,13 @@ from aiogram.types import Message
 
 from handlers.users.help import show_help_text
 from handlers.users.start import show_start_text
-from handlers.users.registration import create_player_handler
+from handlers.users import registration
 from handlers.users.player import show_player_handler, send_place_info
 
 
 async def handle_commands(message: Message, text: str):
     commands = {
-        "/create": create_player_handler,
+        "/create": registration.create_player_handler,
         "/where": send_place_info,
         "/me": show_player_handler,
         "/start": show_start_text,
