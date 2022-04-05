665ee95cdc60308db48d5c69ff2c0287e40cbbb5
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-05 14:55:49 +0300

    Update items.py

--- keyboards/default/items.py
+++ keyboards/default/items.py
@@ -7,3 +7,11 @@ def create_markup(items: list):
         markup.add(KeyboardButton(f'Купити {item["name"]} за {item["price"]}'))
     markup.add(KeyboardButton("Назад"))
     return markup
+
+
+def create_markup_for_types(types: list):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    for type in types:
+        markup.add(KeyboardButton(type))
+    markup.add(KeyboardButton("Назад"))
+    return markup
