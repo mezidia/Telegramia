98adfb69f70e490ba2eec5d01224e1936398cae9
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-06 11:35:32 +0300

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -2,8 +2,10 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from loader import dp
+from keyboards.inline.help_information import create_markup
+from utils.misc.help import help_text
 
 
 @dp.message_handler(CommandHelp())
 async def show_help_text(message: types.Message) -> types.Message:
-    pass
+    return await message.answer(help_text[0], reply_markup=create_markup(0))
