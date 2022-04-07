c3ea66181ff2a03b1af83e724b0b225241dd33c0
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-07 09:48:31 +0300

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,9 +2,12 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
+from utils.misc.help import help_text
+from keyboards.inline.help_information import create_markup
 
 
 @dp.message_handler(CommandStart())
 async def show_start_text(message: types.Message) -> types.Message:
-    text = "Вітаю у текстовій грі <b>Telegramia</b>. Створіть <i>свого героя</i> за допомогою команди /create. Дізнайтесь, як грати, через команду /help"
-    return await message.answer(text)
+    text = "Вітаю у текстовій грі <b>Telegramia</b>. Створіть <i>свого героя</i> за допомогою команди /create. Для того, щоб дізнатись, як грати, прочитайте інструкцію нище."
+    await message.answer(text)
+    return await message.answer(help_text[0], reply_markup=create_markup(0))
