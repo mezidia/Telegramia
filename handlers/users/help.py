8d99c47359e510a8e05cebcf1538408fbb8799d7
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-07 09:43:02 +0300

    Create simple keyboard for help command

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -2,10 +2,9 @@ from aiogram.types import Message
 from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from loader import dp
-from keyboards.inline.help_information import create_markup
-from utils.misc.help import help_text
+from keyboards.inline.help_information import help_markup
 
 
 @dp.message_handler(CommandHelp())
 async def show_help_text(message: Message) -> Message:
-    return await message.answer(help_text[0], reply_markup=create_markup(0))
+    return await message.answer("Уся інструкція доступна на сайті", reply_markup=help_markup)
--- keyboards/inline/help_information.py
+++ keyboards/inline/help_information.py
@@ -4,6 +4,13 @@ from keyboards.inline.callback_datas import help_callback
 from utils.misc.help import help_text
 
 
+help_markup = InlineKeyboardMarkup(row_width=1, inline_keyboard=[
+    [
+        InlineKeyboardButton(text="Посилання на сайт", url="https://google.com") # TODO: change the url
+    ]
+])
+
+
 def create_markup(current_page: int):
     markup = InlineKeyboardMarkup(row_width=3)
     if (current_page - 1) in help_text:
