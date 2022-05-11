d4eff9155811b5751b2df54b0b9cf228a43f7b44
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-11 16:38:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,64 +1,66 @@
-import asyncio
 import logging
+import os
 
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils.executor import start_polling, start_webhook
 
 from tgbot.config import load_config
 from tgbot.filters.is_player import IsPlayer
-from tgbot.handlers.errors import register_error
-from tgbot.handlers.cancel import register_cancel
-from tgbot.handlers.echo import register_echo
-from tgbot.handlers.callbacks import register_callbacks
-from tgbot.handlers.city import register_city_object
-from tgbot.handlers.help import register_help_text
-from tgbot.handlers.horse import register_answer_horse
-from tgbot.handlers.item import register_item_answers
-from tgbot.handlers.player import register_player
-from tgbot.handlers.registration import register_registration
-from tgbot.handlers.road import register_road
-from tgbot.handlers.start import register_start
-from tgbot.handlers.delete import register_delete_handler
 from tgbot.services.setting_commands import set_default_commands
+from loader import dp
 
 logger = logging.getLogger(__name__)
 
 
-def register_all_middlewares(dp):
+def register_all_middlewares(dispatcher: Dispatcher):
     # TODO: set antispamm middleware
     # TODO: set ACL middleware
-    pass
+    logger.info('Registering middlewares')
 
 
-def register_all_filters(dp):
-    dp.filters_factory.bind(IsPlayer)
+def register_all_filters(dispatcher: Dispatcher):
+    logger.info('Registering filters')
+    dispatcher.filters_factory.bind(IsPlayer)
 
 
-def register_all_handlers(dp):
-    handlers = [
-        register_player,
-        register_start,
-        register_cancel,
-        register_registration,
-        register_help_text,
-        register_delete_handler,
-        register_road,
-        register_answer_horse,
-        register_item_answers,
-        register_city_object,
-        register_callbacks,
-        register_echo,
-        register_error,
-    ]
-    for handler in handlers:
-        handler(dp)
+def register_all_handlers(dispatcher: Dispatcher):
+    from tgbot import handlers
+    logger.info('Registering handlers')
+
+
+async def register_all_commands(dispatcher: Dispatcher) -> None:
+    logger.info('Registering commands')
+    await set_default_commands(dispatcher.bot)
+
+
+async def on_startup(dispatcher: Dispatcher, webhook_url: str = None) -> None:
+    register_all_middlewares(dispatcher)
+    register_all_filters(dispatcher)
+    register_all_handlers(dispatcher)
+    await register_all_commands(dispatcher)
+    # Get current webhook status
+    webhook = await dispatcher.bot.get_webhook_info()
+
+    if webhook_url:
+        await dispatcher.bot.set_webhook(webhook_url)
+        logger.info('Webhook was set')
+    elif webhook.url:
+        await dispatcher.bot.delete_webhook()
+        logger.info('Webhook was deleted')
+    logger.info('Bot started')
+
+
+async def on_shutdown(dispatcher: Dispatcher) -> None:
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
+    logger.info('Bot shutdown')
 
 
 async def set_all_default_commands(bot: Bot):
     await set_default_commands(bot)
 
 
-async def main():
+if __name__ == '__main__':
     logging.basicConfig(
         level=logging.INFO,
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
@@ -66,29 +68,28 @@ async def main():
     logger.info("Starting bot")
     config = load_config()
 
-    storage = MemoryStorage()
-    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
-    dp = Dispatcher(bot, storage=storage)
+    # Webhook settings
+    HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+    WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+    WEBHOOK_PATH = f'/webhook/{config.tg_bot.token}'
+    WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+    # Webserver settings
+    WEBAPP_HOST = '0.0.0.0'
+    WEBAPP_PORT = int(os.getenv('PORT', 5000))
 
-    bot['config'] = config
+    start_polling(
+        dispatcher=dp,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+    )
 
-    register_all_middlewares(dp)
-    register_all_filters(dp)
-    register_all_handlers(dp)
-
-    await set_all_default_commands(bot)
-
-    # start
-    try:
-        await dp.start_polling()
-    finally:
-        await dp.storage.close()
-        await dp.storage.wait_closed()
-        # await bot.session.close()
-
-
-if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        logger.error("Bot stopped!")
+    # start_webhook(
+    #     dispatcher=dp,
+    #     on_startup=functools.partial(on_startup, webhook_url=WEBHOOK_URL),
+    #     on_shutdown=on_shutdown,
+    #     webhook_path=WEBHOOK_PATH,
+    #     skip_updates=True,
+    #     host=WEBAPP_HOST,
+    #     port=WEBAPP_PORT
+    # )
