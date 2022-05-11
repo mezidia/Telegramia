22ffa6f9270bb274834696754a3fcd58947423f5
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-11 16:57:13 +0300

    Update start.py

--- tgbot/handlers/start.py
+++ tgbot/handlers/start.py
@@ -3,14 +3,12 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 
 from tgbot.misc.help import help_text
 from tgbot.keyboards.inline.help_information import create_markup
+from loader import dp
 
 
+@dp.message_handler(CommandStart(), state="*")
 async def show_start_text(message: types.Message) -> types.Message:
     text = "Вітаю у текстовій грі <b>Telegramia</b>. Створіть <i>свого героя</i> за допомогою команди /create. Для " \
            "того, щоб дізнатись, як грати, прочитайте інструкцію нище. "
     await message.answer(text)
     return await message.answer(help_text[0], reply_markup=create_markup(0))
-
-
-def register_start(dp: Dispatcher):
-    dp.register_message_handler(show_start_text, CommandStart(), state="*")
