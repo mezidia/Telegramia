ac55265a6a5b76b144becb42fbe3f88d790b12b1
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-11 16:51:04 +0300

    Update help.py

--- tgbot/handlers/help.py
+++ tgbot/handlers/help.py
@@ -3,11 +3,9 @@ from aiogram.types import Message
 from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from tgbot.keyboards.inline.help_information import help_markup
+from loader import dp
 
 
+@dp.message_handler(CommandHelp(), state="*")
 async def show_help_text(message: Message) -> Message:
     return await message.answer("Уся інструкція доступна на сайті", reply_markup=help_markup)
-
-
-def register_help_text(dp: Dispatcher):
-    dp.register_message_handler(show_help_text, CommandHelp(), state="*")
