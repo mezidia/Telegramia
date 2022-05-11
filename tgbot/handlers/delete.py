0d2a0d7e1c8e780ad807573071ac6299364797e4
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-05-11 16:49:58 +0300

    Update delete.py

--- tgbot/handlers/delete.py
+++ tgbot/handlers/delete.py
@@ -4,13 +4,12 @@ from aiogram.types import Message
 from tgbot.config import Config
 from tgbot.models.database import Client
 
+from loader import dp
 
+
+@dp.message_handler(commands=["delete"], state='*')
 async def delete_handler(message: Message) -> Message:
     config: Config = message.bot.get('config')
     client = Client(config.db.password)
     client.delete({'user_id': message.from_user.id}, 'players')
     return await message.answer('Ви успішно видалили свій профіль!')
-
-
-def register_delete_handler(dp: Dispatcher):
-    dp.register_message_handler(delete_handler, commands=["delete"], state='*')
