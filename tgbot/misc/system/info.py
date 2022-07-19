92546f0510bea3e434f1f6493ffd5f8ae7d43006
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-07-19 09:28:09 +0300

    Update info.py
    Make better representation

--- tgbot/misc/system/info.py
+++ tgbot/misc/system/info.py
@@ -1,4 +1,4 @@
-from aiogram.utils.markdown import hitalic
+from aiogram.utils.markdown import hitalic, hbold
 
 
 async def prepare_player_info(data):
@@ -8,11 +8,20 @@ async def prepare_player_info(data):
         for item in data["items"]:
             items += f"{item}, "
     text = (
-        f'Ігрове ім\'я: {hitalic(data["name"])}\n🎖Рівень: {hitalic(data["level"])}\n🌟Досвід: {hitalic(data["experience"])}\n❤Здоров\'я: '
-        f'{hitalic(data["health"])}\nЕнергія: {hitalic(data["energy"])}\n\n💪Сила: {hitalic(data["strength"])}\n⚡Спритність: {hitalic(data["agility"])}\n'
-        f'🎯Інтуїція: {hitalic(data["intuition"])}\n🎓Інтелект: {hitalic(data["intelligence"])}\n💟Клас: {hitalic(data["hero_class"])}\n\n'
-        f'🤝Нація: {hitalic(data["nation"])}\n💰Гроші: {hitalic(data["money"])}\n🎒Речі: {hitalic(items)}\n'
-        f'🐺Транспорт: {hitalic(data["mount"]["name"] if data["mount"] else "немає")}\n'
-        f'\nПоточне місце: {hitalic(data["current_state"])}'
+        f'Ігрове ім\'я: {hbold(data["name"])}\n'
+        f'🎖Рівень: {hitalic(data["level"])}\n'
+        f'🌟Досвід: <i>{data["experience"]:2f}</i>\n'
+        f'❤Здоров\'я: {hitalic(data["health"])}\n'
+        f'Енергія: {hitalic(data["energy"])}\n\n'
+        f'💪Сила: {hitalic(data["strength"])}\n'
+        f'⚡Спритність: {hitalic(data["agility"])}\n'
+        f'🎯Інтуїція: {hitalic(data["intuition"])}\n'
+        f'🎓Інтелект: {hitalic(data["intelligence"])}\n'
+        f'💟Клас: {hitalic(data["hero_class"])}\n\n'
+        f'🤝Нація: {hitalic(data["nation"])}\n'
+        f'💰Гроші: {hitalic(data["money"])}\n'
+        f'🎒Речі: {hitalic(items)}\n'
+        f'🐺Транспорт: {hitalic(data["mount"]["name"] if data["mount"] else "немає")}\n\n'
+        f'Поточне місце: {hbold(data["current_state"])}'
     )
     return text
